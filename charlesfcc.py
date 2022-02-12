eae01340e24bc2496d98efe91b8d18c0b9cbe947
coder7874 <gopani7874@gmail.com>
2022-02-12 16:29:37 +0530

    charlse

---
+++ charlesfcc.py
@@ -0,0 +1,19 @@
+from math import floor
+
+#ELEVETOR FLOOR CONVERSION APP
+
+#You are in Eourope and lost -- and You want to know what floor i will be if i were in US??
+
+# Europe floor ? 0- Ground floor
+# US floor ? 1    - Ground floor
+
+
+eu_floor = int(input('Enter Eroupe Floor? '))
+usa_floor = eu_floor + 1
+
+print(usa_floor)
+
+
+
+
+
