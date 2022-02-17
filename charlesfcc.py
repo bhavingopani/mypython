3f53c059e251b6b717f78c80e0b6aed205824a8d
coder7874 <gopani7874@gmail.com>
2022-02-17 09:37:08 +0530

    charle

---
+++ charlesfcc.py
@@ -0,0 +1,32 @@
+from math import floor
+
+#ELEVETOR FLOOR CONVERSION APP
+
+#You are in Eourope and lost -- and You want to know what floor i will be if i were in US??
+
+# Europe floor ? 0- Ground floor
+# US floor ? 1    - Ground floor
+
+
+# eu_floor = int(input('Enter Eroupe Floor? '))
+# usa_floor = eu_floor + 1
+
+# print(usa_floor)
+
+
+#
+# word = "bananana"
+# i = word.find("na")
+# print(i)
+
+#dictionaries
+
+purse = dict()
+purse['money'] =12
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse)
+
+
+
+
